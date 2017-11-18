d67fcfe34e80f66d0fb4c2ac4072516914f4fb50
Anil <anil.techximum@gmail.com>
2017-11-18 11:33:13 +0530

    partner list and add partner controller added

---
+++ MWM/Controller/AddPartnerTableViewController.swift
@@ -0,0 +1,27 @@
+//
+//  AddPartnerTableViewController.swift
+//  MWM
+//
+//  Created by admin on 18/11/17.
+//  Copyright © 2017 Techximum. All rights reserved.
+//
+
+import UIKit
+
+class AddPartnerTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}

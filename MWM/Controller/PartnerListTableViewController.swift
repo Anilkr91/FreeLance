c819df5158f9a5979b2b2f5c6c70bab774aac2e0
Anil <anil.techximum@gmail.com>
2017-11-18 23:23:05 +0530

    search implemented in partner listing

--- MWM/Controller/PartnerListTableViewController.swift
+++ MWM/Controller/PartnerListTableViewController.swift
@@ -8,21 +8,55 @@
 
 import UIKit
 
-class PartnerListTableViewController: BaseTableViewController {
+class PartnerListTableViewController: BaseTableViewController, UISearchResultsUpdating, UISearchBarDelegate {
     
-    
+    let searchController = UISearchController(searchResultsController: nil)
+    let user = LoginUtils.getCurrentUser()!
     var array: [PartnerModel] = []
+    var filteredArray = [PartnerModel]()
+    var categoryId: Int?
+    var brandName: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        filteredArray = array
+        
         let rightBarButton = UIBarButtonItem(title: "Add", style: UIBarButtonItemStyle.plain, target: self, action: #selector(PartnerListTableViewController.addPartnerSegue))
         self.navigationItem.rightBarButtonItem = rightBarButton
         
         getCategory()
-//        getPartnerList()
+        setupSearchController()
     }
-
+    
+    func setupSearchController() {
+        
+        searchController.searchResultsUpdater = self
+        self.searchController.searchBar.delegate = self
+        searchController.dimsBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search by PartnerName"
+        searchController.searchBar.sizeToFit()
+        searchController.searchBar.becomeFirstResponder()
+        definesPresentationContext = true
+        tableView.tableHeaderView = searchController.searchBar
+        
+        
+        
+        
+        
+        
+//        self.searchController.searchResultsUpdater = self
+//        
+//        self.searchController.hidesNavigationBarDuringPresentation = false
+//        self.searchController.dimsBackgroundDuringPresentation = true
+//        self.searchController.obscuresBackgroundDuringPresentation = false
+//        
+//        searchController.searchBar.sizeToFit()
+//        searchController.searchBar.becomeFirstResponder()
+//        self.navigationItem.titleView = searchController.searchBar
+        
+    }
+    
     func addPartnerSegue() {
         
         performSegue(withIdentifier: "showAddPartnerSegue", sender: self)
@@ -31,25 +65,25 @@ class PartnerListTableViewController: BaseTableViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return array.count
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! PartnerTableViewCell
         
         cell.info = array[indexPath.row]
-
+        
         return cell
     }
     
@@ -68,19 +102,43 @@ class PartnerListTableViewController: BaseTableViewController {
         for category in categories.enumerated() {
             
             if category.element.name == "MBKRestaurant" {
-                getPartnerList(categoryId: category.element.id)
+                brandName = category.element.name
+                categoryId = category.element.id
+                getPartnerList()
             }
         }
     }
     
-    func getPartnerList(categoryId: Int) {
-         let user = LoginUtils.getCurrentUser()!
+    func getPartnerList() {
         
-        let param = ["pageNumber": 0, "pageSize" : 10, "city": user.city!, "categoryId": categoryId] as [String : Any]
+        let param = ["pageNumber": 0, "pageSize" : 10, "city": user.city!, "categoryId": categoryId!] as [String : Any]
         PartnerGetService.executeRequest(param) { (response) in
-
-            print(response)
             
+//            print(response)
+            self.array = response
+            self.tableView.reloadData()
+        }
+    }
+    
+    func updateSearchResults(for searchController: UISearchController) {
+//         If we haven't typed anything into the search bar then do not filter the results
+        if searchController.searchBar.text! == "" {
+            getPartnerList()
+        } else {
+            return
+        }
+    }
+    
+   
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        print("search from keyboard")
+        
+        let param = ["pageNumber": 0, "pageSize" : 10, "city": user.city!, "categoryId": categoryId!, "partnerName":searchBar.text!, "brandName": brandName] as [String : Any]
+        PartnerGetService.executeRequest(param) { (response) in
+            
+            print(response)
             self.array = response
             self.tableView.reloadData()
         }

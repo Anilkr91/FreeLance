42e09079ab76c1cf108658ee8b97275fadd4c686
Anil <anil.techximum@gmail.com>
2017-11-18 11:35:22 +0530

    add partner button added in partner list

--- MWM/Controller/AddPartnerTableViewController.swift
+++ MWM/Controller/AddPartnerTableViewController.swift
@@ -9,19 +9,23 @@
 import UIKit
 
 class AddPartnerTableViewController: UITableViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        let rightBarButton = UIBarButtonItem(title: "Add", style: UIBarButtonItemStyle.plain, target: self, action: #selector(AddPartnerTableViewController.addPartnerSegue))
+        self.navigationItem.rightBarButtonItem = rightBarButton
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    func addPartnerSegue() {
+        
+        print("add patner segue")
     }
 }
